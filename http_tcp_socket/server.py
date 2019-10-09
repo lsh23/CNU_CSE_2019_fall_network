2a0e0de248729cef9fb43e64a83912525604e926
lsh23 <shseoul14@gmail.com>
2019-10-09 21:16:33 +0900

    Complete accept_client function
    accept_client function 1차 완성
    과제 제출용으로는 완성이지만
    실제로 splited 한다음 메소드, url, 프로토콜을 확인한 뒤
    해당 조건에 맞게 처리해주는 식으로 작성하는 것이 완성도가 있는 코드

--- http_tcp_socket/server.py
+++ http_tcp_socket/server.py
@@ -10,6 +10,38 @@ def open_server_socket(IP):
 def accept_client(s_socket):
     print("server_open...")
 
+    while True:
+        conn, addr = s_socket.accept()
+        print("connected by ({0}) , {1}".format(addr[0],addr[1]))
+        print("connect client... ",s_socket.getsockname()[1])
+        data = conn.recv(1024).decode('utf-8')
+        print("accept client socket")
+        print("IP:{0}, data:{1}".format(addr[0],data))
+        splited_data = data.split(" ")
+        method = splited_data[0]
+        # request_url = splited_data[1]
+        # protocol = splited_data[2].split("\r")[0]
+        # print("[{0}, {1}, {2}]".format(method,request_url,protocol))
+        message = "HTTP/1.1 200 0K\r\n"
+        html = """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Page Title</title>
+            </head>
+         <body>
+
+            <h1>This is a Heading</h1>
+            <p>This is a paragraph.</p>
+         </body>
+        </html>
+        """
+        conn.sendall((message+html).encode('utf-8'))
+        
+        print(html)
+        print("Send OK")
+        conn.close()
+
 def main(FLAGS):
     server_socket = open_server_socket(FLAGS)
     accept_client(server_socket)
