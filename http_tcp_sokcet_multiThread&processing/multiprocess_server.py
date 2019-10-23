fab8b5b4a43440e9a5a08d7b575f03275a19d1ce
lsh23 <shseoul14@gmail.com>
2019-10-23 23:31:00 +0900

    Add send_recv function
    send_recv에 해당하는 함수 구현 및 
    main 문에 소켓 통신 사용하는 부분 추가.

--- http_tcp_sokcet_multiThread&processing/multiprocess_server.py
+++ http_tcp_sokcet_multiThread&processing/multiprocess_server.py
@@ -3,7 +3,25 @@ import socket
 import os
 
 def send_recv(client, address):
+    msg = client.recv(1024)
+    print("[client {} {}]".format(os.getpid(),msg.decode()))
+    message = "HTTP/1.1 200 0K\r\n"
+    html = """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Page Title</title>
+            </head>
+         <body>
 
+            <h1>This is a Heading</h1>
+            <p>This is a paragraph.</p>
+         </body>
+        </html>
+        """
+        
+    client.send((message+html).encode('utf-8'))
+    client.close()
 
 
 def main(FLAGS):
@@ -13,8 +31,11 @@ def main(FLAGS):
     clients = list()
 
     while True:
-        #send and recv
-
+        client, address = serversocket.accept()
+        print("accept client from", address)
+        proc = Process(target = send_recv, args=(client,address))
+        proc.start()
+        proc.join()
 
 if __name__ == "__main__":
     import argparse
